4ef0c6af86e1861b5e64e85cfbfed17207b4a745
weijinhao <18513655955@163.com>
2019-01-05 15:50:05 +0800

    监控任务的生命周期

---
+++ src/main/java/com/weijinhao/TaskLifeCycle/Observable.java
@@ -0,0 +1,18 @@
+package com.weijinhao.TaskLifeCycle;
+
+/**
+ *
+ */
+public interface Observable {
+     enum  Cycle {
+         ready,
+        start,
+        run,
+        end,
+        error;
+    }
+
+    //获取生命周期
+    public Cycle getCycle();
+
+}
---
+++ src/main/java/com/weijinhao/TaskLifeCycle/Task.java
@@ -0,0 +1,8 @@
+package com.weijinhao.TaskLifeCycle;
+
+/**
+ * 我们要运行的核心任务接口
+ */
+public interface Task<T> {
+    public T task();
+}

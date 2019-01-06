7d1a7db0c8dc0e592a630df3c9e67483b77957a4
weijinhao <18513655955@163.com>
2019-01-06 18:31:46 +0800

    自定义线程池

---
+++ src/main/java/com/weijinhao/threadpool/DenyRunnableException.java
@@ -0,0 +1,10 @@
+package com.weijinhao.threadpool;
+
+/**
+ * Created by qd on 2019/1/6.
+ */
+public class DenyRunnableException  extends RuntimeException {
+    public DenyRunnableException(String message) {
+        super(message);
+    }
+}
